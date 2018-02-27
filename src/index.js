af10fd7f280e46bf22ae379be33c4a297a01a20d
myskam7 <mksmflppv98@gmail.com>
2018-02-27 15:01:25 -0500

    package update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    React.createElement('h2', null, 'Hello React'),
+   <h2>Hello React with JSX</h2>,
     document.getElementById('root')
 );
