76ea1d571b9dfb0ca1c444a85ce76fd8f74b8ce6
myskam7 <mksmflppv98@gmail.com>
2018-02-27 12:49:06 -0500

    api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fs from 'fs';
 
+import apiRouter from 'express';
 import config from './config';
 
 const server = express();
@@ -9,10 +10,12 @@ server.get('/', (req, res) => {
     res.send('Hello Express');
 });
 
-//Static middleware to access public folder
+//Express middleware to access folders
+server.use('/api', apiRouter);
 server.use(express.static('public'));
 
 
 server.listen(config.port, () => {
     console.info('Express listening on port ', config.port);
-})
+});
+
