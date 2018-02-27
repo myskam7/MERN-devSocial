ccb21ef23c9d3a479edd2f25e6f9434ab05e1cd0
myskam7 <mksmflppv98@gmail.com>
2018-02-27 14:37:36 -0500

    reactDOM

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    React.createElement('...'),
+    React.createElement('h2', null, 'Hello React'),
     document.getElementById('root')
 );
