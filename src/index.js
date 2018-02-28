1361b7decc19184d209b2a5de7a2cae97492a2c1
myskam7 <mksmflppv98@gmail.com>
2018-02-28 13:34:06 -0500

    react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
 
+const color = Math.random() > 0.5 ? 'green' : 'red';
+
 ReactDOM.render(
-   <h2>Hello React with JSX --<p> {Math.random()}</p></h2>,
+   <h2>
+    Hello React with JSX -- <p style={{color: color}}> {Math.random()}</p>
+   </h2>,
     document.getElementById('root')
 );
