2ee5e112c150f48ebecd41cfbe4d024a09480d0d
myskam7 <mksmflppv98@gmail.com>
2018-02-28 13:30:21 -0500

    react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-   <h2>Hello React with JSX</h2>,
+   <h2>Hello React with JSX --<p> {Math.random()}</p></h2>,
     document.getElementById('root')
 );
