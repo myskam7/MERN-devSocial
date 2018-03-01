97294d50a2ef64f1c356a83d286f03d03c95f307
myskam7 <mksmflppv98@gmail.com>
2018-02-28 21:12:28 -0500

    Header App

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,25 @@ import PropTypes from 'prop-types';
 
 import Header from './Header';
 
-const App = () => {
+class App extends Component {
+    state = {
+            pageHeader: 'Naming Contests'
+        };
+    
+
+    render(){
 
     return (
 
         <div className="App">
-            <Header message="Naming Contests" />
+            <Header message={this.state.pageHeader} />
             
             <div>
-                ...
+               ...
             </div>
         </div>
     );
-   
+  } 
 };
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React, {Component} from 'react'; 
+import React from 'react'; 
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 
 import App from './components/App';
-
 
 
 ReactDOM.render(
