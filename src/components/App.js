126ffa1c25d7edd8119769de2cf3ce95982e5e99
myskam7 <mksmflppv98@gmail.com>
2018-02-28 21:49:50 -0500

    contests

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,21 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './Header';
+import ContestPreview from './ContestPreview'; 
 
 class App extends Component {
     state = {
             pageHeader: 'Naming Contests'
         };
 
-        // componentDidMount(){
-        //     console.log('did mount');
+        componentDidMount(){
+            console.log('did mount');
 
-        // }
-        // componentWillUnmount(){
-        //     console.log('unmounted');
+        }
+        componentWillUnmount(){
+            console.log('unmounted');
 
-        // }
+        }
     
     
     render(){
@@ -23,14 +24,17 @@ class App extends Component {
     return (
 
         <div className="App">
-            <Header message={this.state.pageHeader} />
+            <Header message={this.state.pageHeader} />  
             
             <div>
-               ...
+               {this.props.contests.map(contest => 
+                <ContestPreview {...contest} /> 
+            )};
+               
             </div>
         </div>
     );
   } 
 };
 
-export default App
+export default App;
