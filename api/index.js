e00bf212e9809ec9f61f906ad41f01ecac146b36
myskam7 <mksmflppv98@gmail.com>
2018-03-05 18:38:01 -0500

    api/index.js - reduce array into obj

--- api/index.js
+++ api/index.js
@@ -4,7 +4,12 @@ import data from '../src/testData';
 const router = express.Router();
 
 router.get('/contests', (req, res) => {
-    res.send({ contests: data.contests });
+    res.send({ 
+        contests: data.contests.reduce((obj, contest) => {
+            obj[contest.id] = contest; 
+            return obj; 
+        }, {})
+    });
 });
 
 export default router; 
