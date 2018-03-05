4812a157ddc14c580289160c84aa25c70457f001
myskam7 <mksmflppv98@gmail.com>
2018-03-04 20:38:38 -0500

    ContestPreview;

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -3,18 +3,27 @@ import React, {Component} from 'react';
 
 class ContestPreview extends Component {
 
+    handleClick = () => {
+        console.log(this.props.contestName);
+    }
+
     render() {
         return(
-    <div className="ContestPreview"> 
-        <div className="category-name">
-            {contest.categoryName}
-        </div>
-        <div className="contest-name">
-            {contest.contestName}
-        </div>
-    </div>
+            <div className="ContestPreview" onClick={this.handleClick}> 
+                <div className="category-name">
+                    {contest.categoryName}
+                </div>
+                <div className="contest-name">
+                    {contest.contestName}
+                </div>
+            </div>
         );
     }
 };
+
+ContestPreview.propTypes = {
+    categoryName: React.PropTypes.string.isRequired,
+    contestName: React.PropTypes.string.isRequired
+}
 
 export default ContestPreview;
