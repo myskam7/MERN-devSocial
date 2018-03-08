0841638085a07109ef4602c11ce455348599dff0
myskam7 <mksmflppv98@gmail.com>
2018-03-08 11:59:27 -0500

    src/components/Contest

--- src/components/Contest.js
+++ src/components/Contest.js
@@ -6,14 +6,49 @@ class Contest extends Component {
         return(
             
             <div className="Contest">
-                <div className="contest-description">
-                   {this.props.description}
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">Contest Descrioption</h3>
+                    </div>
+                    <div className="panel-body">
+                        <div className="contest-description">
+                            {this.props.description}
+                        </div>
+                    </div>
                 </div>
-                <div className="home-link link" 
-                      onClick={this.props.contestListClick}> 
-                      Contest List
+               
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">Names Suggested</h3>
+                    </div>
                 </div>
-            </div>
+                <div className="panel-body">
+                    <ul className="list-group">
+                        <li className="list-group-item">Name One...</li>
+                        <li className="list-group-item">Name Two...</li>
+                    </ul>
+                </div>
+
+                <div className="panel panel-info">
+                <div className="panel-heading">
+                    <h3 className="panel-title">Suggest A Name</h3>
+                </div>
+                <div className="panel-body">
+                    <form>
+                        <div className="input-group">
+                            <input type="text" placeholder="New Name Here..." className="form-control" />
+                            <span className="input-group-btn">
+                                <button type="submit" className="btn btn-info">Submit</button>"
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </div>        
+     </div>
+
+            
+
+       
         );
     }
 }
