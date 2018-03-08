8bac9098712e8da146a60c82499aa7204d23e028
myskam7 <mksmflppv98@gmail.com>
2018-03-08 10:05:44 -0500

    add mongoUri in config

--- src/loadTestData.js
+++ src/loadTestData.js
@@ -1,6 +1,6 @@
 import {MongoClient} from 'mongodb';
 import assert from 'assert'; 
-import config from 'config';
+import config from './config';
 
 MongoClient.connect(config.mongodbUri, (err, db) => {
     assert.equal(null,err);
