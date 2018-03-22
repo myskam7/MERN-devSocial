350d17db265306ff8a2e9a7f083982c3ccab2440
myskam7 <mksmflppv98@gmail.com>
2018-03-22 13:23:12 -0400

    updateTestData.js

--- updateTestData.js
+++ updateTestData.js
@@ -1,12 +1,13 @@
-import { MongClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import assert from 'assert';
 import config from './config';
 
-MongClient.connect(config.mongodbUrl, (err, db) => {
+MongoClient.connect(config.mongodbUri, (err, client) => {
     assert.equal(null, err); 
+    const db = client.db('testing');
 
     let contestCount = 0; 
-    db.collection ('contests').find({}).each((err, contest) => {
+    db.collection('contests').find({}).each((err, contest) => {
         assert.equal(null, err); 
         if(!contest) {
             return;
@@ -14,7 +15,7 @@ MongClient.connect(config.mongodbUrl, (err, db) => {
 
         contestCount++; 
         db.collection('names')
-        .find({ id: { $in: contest.namesIds }})
+        .find({ id: { $in: contest.nameIds }})
         .project({ _id: 1})
         .toArray()
         .then(_ids => {
