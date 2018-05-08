c3fb30379cc74f64c1c2340f46ad5659321285d8
myskam7 <mksmflppv98@gmail.com>
2018-05-08 19:01:29 -0400

    add reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
+const middleware = [thunk]; 
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(() => [], {}, applyMiddleware(...middleware));
 
 export default store;
