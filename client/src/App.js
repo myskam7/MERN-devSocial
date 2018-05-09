bfcd5a14d83c557d541988c71324e9cf61a04268
myskam7 <mksmflppv98@gmail.com>
2018-05-09 19:55:40 -0400

    creating front end profile

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import  { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -22,6 +22,16 @@ if(localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   //Set current user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  const currentTime = Date.now() / 1000; 
+  if(decoded.exp < currentTime) {
+    //Logout user 
+    store.dispatch(logoutUser()); 
+    //TODO: Clear current profile
+    //redirect to login page
+    window.location.href = '/login'; 
+  }
 }
 
 class App extends Component {
--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import propTypes, { PropTypes} from 'prop-types';
 
 import { loginUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
     constructor() {
@@ -18,10 +19,17 @@ class Login extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    componentDidMount() {
+      if(this.props.auth.isAuthenticated) {
+        this.props.history.push('/dashboard')
+      }
+    }
+
     componentWillReceiveProps(nextProps) {
       if(nextProps.auth.isAuthenticated) {
-        this.props.history.push('/dashboard');
+        this.props.history.push('/dashboard'); 
       }
+
 
       if(nextProps.errors) {
         this.setState({
@@ -60,26 +68,25 @@ class Login extends Component {
             <h1 className="display-4 text-center">Sign Up</h1>
             <p className="lead text-center">Create your DevConnector account</p>
             <form noValidate onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <input type="email"
-                className={classnames('form-control form-control-lg', {
-                  'is-invalid': errors.email})}
-                 placeholder="Email Address"
-                 name="email"
-                 value={this.state.email}
-                 onChange={this.onChange} />
-                 {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
-              </div>
-              <div className="form-group">
-                <input type="password"
-                className={classnames('form-control form-control-lg', {
-                  'is-invalid': errors.password})}
-                 placeholder="Password
-                " name="password"
-                 value={this.state.password}
-                 onChange={this.onChange} />
-                 {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
-              </div>
+          
+            <TextFieldGroup 
+            placeholder="Email address"
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.onChange}
+            error={errors.email}
+            />
+
+            <TextFieldGroup 
+            placeholder="Passworkd"
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.onChange}
+            error={errors.password}
+            />
+              
               <input type="submit"
                className="btn btn-info btn-block mt-4" />
             </form>
