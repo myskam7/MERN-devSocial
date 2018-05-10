3886f5d9e935c393d4e45802206b081c4339ce5c
myskam7 <mksmflppv98@gmail.com>
2018-05-10 13:50:11 -0400

    form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import  { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 
@@ -12,6 +12,9 @@ import Footer from './components/layout/Footer';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
 import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/common/PrivateRoute'; 
+import CreateProfile from './components/createprofile/CreateProfile';
+
 import store from './store';
 import './App.css';
 
@@ -35,8 +38,8 @@ if(localStorage.jwtToken) {
     store.dispatch(clearCurrentProfile()); 
     //redirect to login page
     window.location.href = '/login'; 
-  }
-}
+  };
+};
 
 class App extends Component {
   render() {
@@ -49,7 +52,15 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
-              <Route exact path="/dashboard" component={ Dashboard } />
+              <Switch>
+              <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+              </Switch> <Switch>
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+            </Switch>
             </div>
             <Footer />    
           </div>
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,36 +1,54 @@
 import axios from 'axios';
 
-import {GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS
+} from './types';
 
-//Get current profile
+// Get current profile
 export const getCurrentProfile = () => dispatch => {
-    dispatch(setProfileLoading());
-    axios.get('/api/profile')
+  dispatch(setProfileLoading());
+  axios
+    .get('/api/profile')
     .then(res =>
-    dispatch({
+      dispatch({
         type: GET_PROFILE,
         payload: res.data
-    })
+      })
     )
-    .catch(err => 
-        dispatch({
-            type: GET_PROFILE,
-            payload: {}
-        })
-    )
-}
-
-// Profile loading 
-export const setProfileLoading = () => {
-    return{
-        type: PROFILE_LOADING 
-
-    };
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
 };
-// Clear current profile 
-export const clearCurrentProfile = () => {
-    return{
-        type: CLEAR_CURRENT_PROFILE
 
-    };
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Profile loading
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
+
+// Clear profile
+export const clearCurrentProfile = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE
+  };
 };
