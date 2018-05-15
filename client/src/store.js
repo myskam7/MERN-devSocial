74eacf6ff124569e4fbbf199e2570490d06f01eb
myskam7 <mksmflppv98@gmail.com>
2018-05-14 20:48:09 -0400

    heroku

--- client/src/store.js
+++ client/src/store.js
@@ -7,7 +7,7 @@ const initialState = {};
 const middleware = [thunk]; 
 
 const store = createStore(rootReducers,
-    initialState
+    initialState,
     compose(applyMiddleware(...middleware)
     // window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__() //REDUXDEVTOOLS
   )
