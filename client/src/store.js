f633792239b9d9425e09126dd7ee402ebc350772
myskam7 <mksmflppv98@gmail.com>
2018-05-14 20:34:18 -0400

    heroku

--- client/src/store.js
+++ client/src/store.js
@@ -4,11 +4,11 @@ import thunk from 'redux-thunk';
 import rootReducers from './reducers';
 
 const initialState = {};
-
+// const middleware = [thunk]; 
 
 const store = createStore(rootReducers,
     initialState,
-    compose(applyMiddleware(...middleware)
+    // compose(applyMiddleware(...middleware)
     // window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__() //REDUXDEVTOOLS
   )
 );
